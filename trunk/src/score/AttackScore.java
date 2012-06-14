61a73471ef158af867d93079274b4ef6754a8f06
renato.ppontes@gmail.com <renato.ppontes@gmail.com@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-06-14 18:23:05 +0000

    scores

---
+++ trunk/src/score/AttackScore.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package score;
+
+/**
+ *
+ * @author Renato
+ */
+public class AttackScore extends Score {
+
+    @Override
+    public double getScore() {
+        return 0.0;
+    }
+    
+}
---
+++ trunk/src/score/EvadeScore.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package score;
+
+/**
+ *
+ * @author Renato
+ */
+public class EvadeScore extends Score {
+    @Override
+    public double getScore() {
+        return 0.0;
+    }
+}
