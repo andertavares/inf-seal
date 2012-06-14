5b7f52fb05838eb7e2f202fac7014da392785c0d
renato.ppontes@gmail.com <renato.ppontes@gmail.com@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-06-14 18:23:05 +0000

    scores

---
+++ src/score/Score.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package score;
+
+/**
+ *
+ * @author Renato
+ */
+public abstract class Score {
+    public abstract double getScore();
+}
