4b149618b02b17a973c4f05cbd5f01150a58624a
renato.ppontes@gmail.com <renato.ppontes@gmail.com@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-06-25 17:38:07 +0000

    Regras fuzzy

--- trunk/src/score/AttackScore.java
+++ trunk/src/score/AttackScore.java
@@ -4,15 +4,34 @@
  */
 package score;
 
+import net.sourceforge.jFuzzyLogic.FIS;
+
 /**
  *
  * @author Renato
  */
 public class AttackScore extends Score {
+    private FIS fis;
+
+    public AttackScore() {
+        super();
+
+        String fileName = "C\\Users\\Renato\\Dev\\demolition\\inf-seal\\src\\score\\attack.fcl";
+        this.fis = FIS.load(fileName, true);
+
+        if (this.fis == null) {
+            System.err.println("Can't load file: '" + fileName + "'");
+        }
+    }
 
     @Override
-    public double getScore() {
-        return 0.0;
+    public double getScore(float angle, float distance, float direction) {
+        this.fis.setVariable("angle", angle);
+        this.fis.setVariable("distance", distance);
+        this.fis.setVariable("direction", direction);
+        this.fis.evaluate();
+
+        return this.fis.getVariable("activate").defuzzify();
     }
     
 }
