258fc96afb47498c7b35126335224346f7fe2ccc
andersonrochatavares <andersonrochatavares@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-07-01 17:25:08 +0000

    Checagem de velocidade adicionada ao retirar aceleracao.

--- src/behavior/StuckInTrack.java
+++ src/behavior/StuckInTrack.java
@@ -15,15 +15,17 @@ public class StuckInTrack extends Behavior {
 	private final int THRESHOLD = 300;
 	
 	Action theAction;
+	private double centerPotential;
 	
 	public StuckInTrack(){
 		stuckFor = 0;
+		centerPotential = 0;
 		theAction = new Action();
 	}
 
 	@Override
 	public double score(SensorModel sensors, Vector<Car> opponentData) {
-		
+		/*
 		 if (isStuck) return 1;
 		 if (stuckFor >= THRESHOLD) {
             isStuck = true;
@@ -36,6 +38,20 @@ public class StuckInTrack extends Behavior {
 	        stuckFor = 0;
 		 
 		 return -1;
+		 */
+		//armazena o ultimo valor
+		centerPotential = centerPotential(sensors);
+		
+		//testa o potencial, se estiver muito proximo, sinaliza como preso
+		if (centerPotential > 0.8) {
+			isStuck = true;
+			return 1;
+		}
+		//se esta preso, espera afastar-se para dizer que nao esta mais preso
+		if(isStuck && centerPotential > 0.2) return 1;
+		
+		isStuck = false;
+		return -1;
 	}
 
 	@Override
@@ -44,20 +60,57 @@ public class StuckInTrack extends Behavior {
 		theAction.gear = -1;
         theAction.accelerate = 1;
         theAction.steering = - getSteer(sensors);
-        
+        /*
         stuckFor--;
         if (stuckFor <= 0){
             isStuck = false;
         }
-        
+        */
         //System.out.println("gear" + sensors.getGear());
         
         return theAction;
 	}
 	
 	public String toString(){
-		return "Stuck";/*. stuckFor=" + stuckFor + ", gear = " + theAction.gear + ", accel= " 
+		return "Stuck in track. Center potential: " + centerPotential ;/*. stuckFor=" + stuckFor + ", gear = " + theAction.gear + ", accel= " 
 		+ theAction.accelerate + ", steer=" + theAction.steering;*/
+	}
+	
+	/**
+	 * Retorna o potencial detectado pelos sensores 'a esquerda
+	 * @param sensors
+	 * @return
+	 */
+	private double leftPotential(SensorModel sensors) {
+		double min = 100;
+		for (int i = 0; i < 6; i++) {
+			if (sensors.getTrackEdgeSensors()[i] < min) min = sensors.getTrackEdgeSensors()[i];
+		}
+		return (1/(Math.sqrt(min)))/1.55;
+	}
+	
+	/**
+	 * Retorna o potencial detectado pelo sensor central
+	 * @param sensors
+	 * @return
+	 */
+	private double centerPotential(SensorModel sensors) {
+		double min = 100;
+		if (sensors.getTrackEdgeSensors()[9] < min) min = sensors.getTrackEdgeSensors()[9];
+		return (1/(min))/0.59;
+	}
+	
+	/**
+	 * Retorna o potencial detectado pelo sensor 'a direita
+	 * @param sensors
+	 * @return
+	 */
+	private double rightPotential(SensorModel sensors) {
+		double min = 100;
+		for (int i = 13; i < 19; i++) {
+			if (sensors.getTrackEdgeSensors()[i] < min) min = sensors.getTrackEdgeSensors()[i];
+		}
+		return (1/(Math.sqrt(min)))/1.55;
 	}
 
 }
