adf5e9a5b0fc539e68ea6bcea02dc1b6e9a3447b
sergio.ms2 <sergio.ms2@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-07-02 02:26:09 +0000

    getSteer agora percorre a pista no sentido anti-horário também.

--- trunk/src/behavior/Behavior.java
+++ trunk/src/behavior/Behavior.java
@@ -18,7 +18,17 @@ public abstract class Behavior {
 	protected float getSteer(SensorModel sensors){
 		// steering angle is compute by correcting the actual car angle w.r.t. to track 
 		// axis [sensors.getAngle()] and to adjust car position w.r.t to middle of track [sensors.getTrackPos()*0.5]
-	    float targetAngle=(float) (sensors.getAngleToTrackAxis()-sensors.getTrackPosition()*0.5);
+		float targetAngle;
+		double ro = sensors.getAngleToTrackAxis();
+		
+		if (Math.abs(ro) > Math.PI/2) { // lado inverso
+			double ang = sensors.getAngleToTrackAxis();
+			if (ang > 0)	ang = ang - Math.PI;
+			else			ang = ang + Math.PI;
+			targetAngle=(float) (ang + sensors.getTrackPosition()*0.5);
+		} else {
+			targetAngle=(float) (sensors.getAngleToTrackAxis()-sensors.getTrackPosition()*0.5);
+		}
 	    // at high speed reduce the steering command to avoid loosing the control
 	    /*if (sensors.getSpeed() > steerSensitivityOffset)
 	        return (float) (targetAngle/(steerLock*(sensors.getSpeed()-steerSensitivityOffset)*wheelSensitivityCoeff));
