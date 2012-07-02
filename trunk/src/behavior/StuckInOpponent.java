eb748c6da277d01c3d6d3e1c1a264305640750eb
sergio.ms2 <sergio.ms2@161dce0b-8ccb-efdd-4249-d40ce7e1c4a5>
2012-07-02 14:30:32 +0000

    Adicionado condicao de stuck frontal.

--- trunk/src/behavior/StuckInOpponent.java
+++ trunk/src/behavior/StuckInOpponent.java
@@ -10,20 +10,34 @@ import sealbot.SensorModel;
 public class StuckInOpponent extends Behavior {
 
 	private long lastDet = 0;
+	boolean latCol = false;
+	boolean frontCol = false;
 	
 	@Override
 	public double score(SensorModel sensors, Vector<Car> opponentData) {
 
 		double s[] = sensors.getOpponentSensors();
-		double sd = 0, ret = -1;
+		double frontMin = 300, latMin = 300, ret = -1;
 		long acTime = 0;
-		boolean cret = false;
-
-		for (int i = 0; i < 12; i++) sd += (300 - s[i]);
-		for (int i = 24; i < 36; i++) sd += (300 - s[i]);
-
-		if (sensors.getSpeed() < 5 && sd > 295) cret = true;
-		if (cret) {
+		frontCol = false;
+		latCol = false;
+		
+		for (int i = 0; i < 12; i++) {
+			if (latMin > s[i])
+				latMin = s[i];
+		}
+		for (int i = 24; i < 36; i++) {
+			if (latMin > s[i])
+				latMin = s[i];
+		}
+		for (int i = 12; i < 24; i++) {
+			if (frontMin > s[i])
+				frontMin = s[i];
+		}
+		
+		// Preso pela lateral ou oponente atacando por tras
+		if (sensors.getSpeed() < 5 && latMin < 5) latCol = true;
+		if (latCol) {
 			acTime = (new Date()).getTime();
 			if (lastDet == 0) {
 				lastDet = acTime;
@@ -33,19 +47,45 @@ public class StuckInOpponent extends Behavior {
 				ret = 0.95;
 			}
 		}
-		else
-			lastDet = 0;
 		
+		// Preso pela frente
+		if (sensors.getSpeed() < 5 && frontMin < 5) frontCol = true;
+		if (frontCol) {
+			acTime = (new Date()).getTime();
+			if (lastDet == 0) {
+				lastDet = acTime;
+				ret = -1;
+			}
+			else if (acTime - lastDet > 500) {
+				ret = 0.95;
+			}
+		}
+		
+		if (!latCol && !frontCol)
+			lastDet = 0;
+
 		//System.out.println(ret);
 
 		return ret;
                 
 	}
 
-	@Override
+	@Override	
 	public Action control(SensorModel sensors) {
 		// TODO Auto-generated method stub
-		Action a = (new Evade()).control(sensors);
+		
+		if (latCol) System.out.println("Lateral stuck");
+		if (frontCol) System.out.println("Front stuck");
+		
+		Action a;
+		if (frontCol) {
+			a = new Action();
+			a.gear = -1;
+			a.accelerate = 1;
+		}
+		else {
+			a = (new Evade()).control(sensors);
+		}
 		return a;
 	}
 	
